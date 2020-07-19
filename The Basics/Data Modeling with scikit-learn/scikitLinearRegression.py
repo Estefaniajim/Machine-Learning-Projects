5d802a941a88c8f4fbe826e4d50fb4fa4abf3ce5
Estefania Jimenez Garcia <31485312+Estefaniajim@users.noreply.github.com>
2020-07-19 17:39:17 -0500

    Update scikitLinearRegression.py

---
+++ The Basics/Data Modeling with scikit-learn/scikitLinearRegression.py
@@ -0,0 +1,21 @@
+from sklearn import linear_model
+import numpy as np
+
+pizza_data = [[2100,  800],
+       [2500,  850],
+       [1800,  760],
+       [2000,  800],
+       [2300,  810]]
+pizza_prices = [10.99, 12.5,  9.99, 10.99, 11.99]
+reg = linear_model.LinearRegression()
+reg.fit(pizza_data, pizza_prices)
+# new pizza data
+new_pizzas = np.array([[2000,  820],
+                       [2200,  830]])
+price_predicts = reg.predict(new_pizzas)
+print('{}\n'.format(repr(price_predicts)))
+print('Coefficients: {}\n'.format(repr(reg.coef_)))
+print('Intercept: {}\n'.format(reg.intercept_))
+# Using previously defined pizza_data, pizza_prices
+r2 = reg.score(pizza_data, pizza_prices)
+print('R2: {}\n'.format(r2))
