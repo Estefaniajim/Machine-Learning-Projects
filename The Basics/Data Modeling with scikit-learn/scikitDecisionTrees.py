cf629778efb24eac42dc7db412049130d9b9890d
Estefania Jimenez Garcia <31485312+Estefaniajim@users.noreply.github.com>
2020-07-29 18:13:25 -0500

    Create scikitDecisionTrees.py

---
+++ The Basics/Data Modeling with scikit-learn/scikitDecisionTrees.py
@@ -0,0 +1,18 @@
+from sklearn import tree
+clf_tree1 = tree.DecisionTreeClassifier()
+reg_tree1 = tree.DecisionTreeRegressor()
+clf_tree2 = tree.DecisionTreeClassifier(
+  max_depth=8)  # max depth of 8
+reg_tree2 = tree.DecisionTreeRegressor(
+  max_depth=5)  # max depth of 5
+
+# predefined dataset
+# print('Data shape: {}\n'.format(data.shape))
+# Binary labels
+# print('Labels:\n{}\n'.format(repr(labels)))
+# clf_tree1.fit(data, labels)
+
+# Example
+# Decision tree with max depth equal to 5
+d_tree = tree.DecisionTreeRegressor(max_depth=5)
+# d_tree.fit(data,labels)
