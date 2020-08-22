7d50452edb15205720d9f123c3e93d63ca9c2696
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-21 21:51:32 -0500

    Update K-Means Clustering.py

--- The Basics/Clustering with scikit-learn/K-Means Clustering.py
+++ The Basics/Clustering with scikit-learn/K-Means Clustering.py
@@ -18,6 +18,7 @@ centroid = cluster.mean(axis=0)
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(data) # predefined data
 # cluster assignments
+# the final cluster assignments for each data observation
 print('{}\n'.format(repr(kmeans.labels_)))
 # array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 #        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -26,3 +27,15 @@ print('{}\n'.format(repr(kmeans.labels_)))
 #        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 0, 2, 2, 2,
 #        2, 2, 2, 0, 0, 2, 2, 2, 2, 0, 2, 0, 2, 0, 2, 2, 0, 0, 2, 2, 2, 2,
 #        2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 0], dtype=int32)
+# centroids
+print('{}\n'.format(repr(kmeans.cluster_centers_)))
+# array([[5.9016129 , 2.7483871 , 4.39354839, 1.43387097],
+#        [5.006     , 3.418     , 1.464     , 0.244     ],
+#        [6.85      , 3.07368421, 5.74210526, 2.07105263]])
+# Now we make the prediction
+new_obs = np.array([
+  [5.1, 3.2, 1.7, 1.9],
+  [6.9, 3.2, 5.3, 2.2]])
+# predict clusters
+print('{}\n'.format(repr(kmeans.predict(new_obs))))
+# array([1, 2], dtype=int32)
