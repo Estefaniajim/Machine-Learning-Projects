892f0320dc32579310fee1053a650158a6a3ade1
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-29 19:23:07 -0500

    Update HierarchicalClustering.py

--- The Basics/Clustering with scikit-learn/HierarchicalClustering.py
+++ The Basics/Clustering with scikit-learn/HierarchicalClustering.py
@@ -1,1 +1,7 @@
+from sklearn.cluster import AgglomerativeClustering
 # Agglomerative clustering
+agg = AgglomerativeClustering(n_clusters=3)
+# predefined data
+agg.fit(data)
+# cluster assignments
+print('{}\n'.format(repr(agg.labels_)))
