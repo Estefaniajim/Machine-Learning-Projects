04535496768c366738e3218d40d3f08b0a639372
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-29 21:07:27 -0500

    Update FeatureClustering.py

--- The Basics/Clustering with scikit-learn/FeatureClustering.py
+++ The Basics/Clustering with scikit-learn/FeatureClustering.py
@@ -1,1 +1,9 @@
 from sklearn.cluster import FeatureAgglomeration
+
+print('Original shape: {}\n'.format(data.shape))
+print('First 10:\n{}\n'.format(repr(data[:10])))
+
+agg = FeatureAgglomeration(n_clusters=2)
+new_data = agg.fit_transform(data)
+print('New shape: {}\n'.format(new_data.shape))
+print('First 10:\n{}\n'.format(repr(new_data[:10])))
