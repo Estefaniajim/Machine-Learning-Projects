e6dacf0233a7ddebecb7463dd0c4cdd0492f6034
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-29 20:57:24 -0500

    Update EvaluatingClusters.py

--- The Basics/Clustering with scikit-learn/EvaluatingClusters.py
+++ The Basics/Clustering with scikit-learn/EvaluatingClusters.py
@@ -51,28 +51,34 @@ pred_labels = np.array([0, 0, 1, 1, 2, 2])
 
 ami = adjusted_mutual_info_score(true_labels, pred_labels)
 print('{}\n'.format(ami))
+# 0.2250422831983088
 
 # symmetric
 ami = adjusted_mutual_info_score(pred_labels, true_labels)
 print('{}\n'.format(ami))
+# 0.2250422831983088
 
 # Perfect labeling
 perf_labels = np.array([0, 0, 0, 1, 1, 1])
 ami = adjusted_mutual_info_score(true_labels, perf_labels)
 print('{}\n'.format(ami))
+# 1.0
 
 # Perfect labeling, permuted
 permuted_labels = np.array([1, 1, 1, 0, 0, 0])
 ami = adjusted_mutual_info_score(true_labels, permuted_labels)
 print('{}\n'.format(ami))
+# 1.0
 
 renamed_labels = np.array([1, 1, 1, 3, 3, 3])
 # Renamed labels to 1, 3
 ami = adjusted_mutual_info_score(true_labels, renamed_labels)
 print('{}\n'.format(ami))
+# 1.0
 
 true_labels2 = np.array([0, 1, 2, 0, 3, 4, 5, 1])
 # Bad labeling
 pred_labels2 = np.array([1, 1, 0, 0, 2, 2, 2, 2])
 ami = adjusted_mutual_info_score(true_labels2, pred_labels2)
 print('{}\n'.format(ami))
+# -0.10526315789473674
