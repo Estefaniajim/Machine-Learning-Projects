341dbc8ffa439e97b2cb9ae6a70f14ac14fa86e2
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-29 20:45:22 -0500

    Update EvaluatingClusters.py

--- The Basics/Clustering with scikit-learn/EvaluatingClusters.py
+++ The Basics/Clustering with scikit-learn/EvaluatingClusters.py
@@ -1,6 +1,9 @@
 from sklearn.metrics import adjusted_rand_score # ARI
 from sklearn.metrics import adjusted_mutual_info_score # AMI
+import numpy as np
 
 # ARI
+true_labels = np.array([0, 0, 0, 1, 1, 1])
+pred_labels = np.array([0, 0, 1, 1, 2, 2])
 
 # AMI
