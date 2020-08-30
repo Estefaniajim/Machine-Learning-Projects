a588034fad84830855672908068d11ae64c360eb
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-29 20:51:36 -0500

    Update EvaluatingClusters.py

--- The Basics/Clustering with scikit-learn/EvaluatingClusters.py
+++ The Basics/Clustering with scikit-learn/EvaluatingClusters.py
@@ -8,30 +8,36 @@ pred_labels = np.array([0, 0, 1, 1, 2, 2])
 
 ari = adjusted_rand_score(true_labels, pred_labels)
 print('{}\n'.format(ari))
+# 0.24242424242424246
 
 # symmetric
 ari = adjusted_rand_score(pred_labels, true_labels)
 print('{}\n'.format(ari))
+# 0.24242424242424246
 
 # Perfect labeling
 perf_labels = np.array([0, 0, 0, 1, 1, 1])
 ari = adjusted_rand_score(true_labels, perf_labels)
 print('{}\n'.format(ari))
+# 1.0
 
 # Perfect labeling, permuted
 permuted_labels = np.array([1, 1, 1, 0, 0, 0])
 ari = adjusted_rand_score(true_labels, permuted_labels)
 print('{}\n'.format(ari))
+# 1.0
 
 renamed_labels = np.array([1, 1, 1, 3, 3, 3])
 # Renamed labels to 1, 3
 ari = adjusted_rand_score(true_labels, renamed_labels)
 print('{}\n'.format(ari))
+# 1.0
 
 true_labels2 = np.array([0, 1, 2, 0, 3, 4, 5, 1])
 # Bad labeling
 pred_labels2 = np.array([1, 1, 0, 0, 2, 2, 2, 2])
 ari = adjusted_rand_score(true_labels2, pred_labels2)
 print('{}\n'.format(ari))
+# -0.12903225806451613
 
 # AMI
