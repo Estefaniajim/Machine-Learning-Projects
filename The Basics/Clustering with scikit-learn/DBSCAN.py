7fa1768f057267382ac6b78ac4f83ccedff943c4
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-29 20:09:29 -0500

    Update DBSCAN.py

--- The Basics/Clustering with scikit-learn/DBSCAN.py
+++ The Basics/Clustering with scikit-learn/DBSCAN.py
@@ -1,1 +1,4 @@
 from sklearn.cluster import DBSCAN
+
+# ε maximum distance between two data
+dbscan = DBSCAN(eps=1.2, min_samples=30) # eps = ε, min_samples, min_samples = minimum size of a core sample's neighborhood
