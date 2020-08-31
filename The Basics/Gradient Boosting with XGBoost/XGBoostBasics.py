ff5429f96c6b8038c7f72409a11c355323d8b481
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-08-31 17:58:48 -0500

    Update XGBoostBasics.py

---
+++ The Basics/Gradient Boosting with XGBoost/XGBoostBasics.py
@@ -0,0 +1,10 @@
+import xgboost as xgb
+import numpy as np
+
+# DMatrix
+data = np.array([
+  [1.2, 3.3, 1.4],
+  [5.1, 2.2, 6.6]])
+dmat1 = xgb.DMatrix(data)
+labels = np.array([0, 1])
+dmat2 = xgb.DMatrix(data, label=labels)
