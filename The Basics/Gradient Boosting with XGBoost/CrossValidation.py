525075af24dc0c31f500679a0a758d08289cf603
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-09-05 12:58:20 -0500

    Create CrossValidation.py

---
+++ The Basics/Gradient Boosting with XGBoost/CrossValidation.py
@@ -0,0 +1,11 @@
+import xgboost as xgb
+
+# predefined data and labels
+dtrain = xgb.DMatrix(data, label=labels)
+params = {
+  'max_depth': 2,
+  'lambda': 1.5,
+  'objective':'binary:logistic'
+}
+cv_results = xgb.cv(params, dtrain)
+print('CV Results:\n{}'.format(cv_results))
