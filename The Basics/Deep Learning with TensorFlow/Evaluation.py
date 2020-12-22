8c1c669d3a7d6114ae54fff14b58b4c25aa3bf16
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2020-12-21 18:00:47 -0600

    Update Evaluation.py

--- The Basics/Deep Learning with TensorFlow/Evaluation.py
+++ The Basics/Deep Learning with TensorFlow/Evaluation.py
@@ -1,2 +1,7 @@
 import tensorflow as tf
 
+# Evaluation of the accuracy in the pretrained model on the test data and labels
+# test_data, test_labels, inputs, labels, accuracy
+# are all predefined in the backend
+feed_dict = {inputs:test_data, labels:test_labels}
+eval_acc = sess.run(accuracy, feed_dict=feed_dict)
